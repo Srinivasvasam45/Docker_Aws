a78096a40abd43f1a350943fd439298fb2a92971
Srinivasvasam45 <srinivasvasam15@gmail.com>
2026-09-02 07:29:09 +0530

    initial commit

---
+++ Backend/server.js
@@ -0,0 +1,35 @@
+import express from 'express'
+import {createServer} from 'http'
+import {Server } from 'socket.io'
+import {YSocketIO} from "y-socket.io/dist/server"
+
+
+
+const app =  express()
+app.use(express.static('public'))
+
+const httpServer = createServer(app)
+
+const io = new Server(httpServer,{
+    cors:{
+        origin:"*",
+        methods: ["GET","POST"]
+    }
+})
+
+const ySocketIo = new YSocketIO(io)
+ySocketIo.initialize()
+
+
+
+app.get('/health', (req, res) =>{
+    res.status(200).json({
+        message: "ok",
+        success:true
+    })
+})
+
+
+httpServer.listen(3000, () =>{
+    console.log("Server is running on port 3000")
+})
